eec2a4f5455cd52dc09caa85e061439e0c521d57
Luis Del Águila <luisdelaguila@outlook.com>
2019-12-14 11:40:11 -0600

    Make progress on day 8

--- 8/script.scm
+++ 8/script.scm
@@ -1,21 +1,26 @@
 (import (chicken file posix))
+
 ;; Open the file "read only"
 (define infn (file-open "./input.txt" open/rdonly))
-
 (define columnCount 25)
-(define rowCount 6)
+(define lastRow 5)
+; (define lastLayer )
 
 ;; read some text from the file
-(define readz
-  (lambda ()
-    (car (file-read infn columnCount))))
+(define getRow
+  (lambda (columnCount)
+    (define result (file-read infn columnCount))
+    (define readChars (car (cdr result)))
+    (print readChars)
+    (define text (car result))))
 
 (define printRows
-  (lambda (rowCount)
+  (lambda (currentRow)
+    (print (getRow columnCount))
     (cond
-      (zero? rowCount) (print done)
-      (else (printRows (sub1 rowCount))))))
+      ((zero? currentRow) #t)
+      (else (printRows (sub1 currentRow))))))
 
 (define (main args)
-  (printRows rowCount)
+  (printRows lastRow)
   (file-close infn))
