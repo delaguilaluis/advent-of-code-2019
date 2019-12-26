7c67ee05e1170d388fd56c1c95c7ad9db18aa996
Luis Andrés Del Águila <ldelaguila@paypal.com>
2019-12-25 21:42:20 -0600

    Complete solution for day 8 (part 1)

--- 8/script.scm
+++ 8/script.scm
@@ -27,22 +27,12 @@
         (charList->numberList (string->list (car result))))
       (else (quote ())))))
 
-; (define printLayers
-;   (lambda (size)
-;     (define layer (getLayer size))
-;     (cond
-;       ((null? layer) #t)
-;       (else
-;         (print layer)
-;         (printLayers size)))))
-
-; get the max value out of a list of numbers
-(define max
-  (lambda (lat maxVal)
-    (cond 
-      ((null? lat) maxVal)
-      ((> (car lat) maxVal) (max (cdr lat) (car lat)))
-      (else (max (cdr lat) maxVal)))))
+(define getLayers
+  (lambda (layerSize)
+    (define layer (getLayer layerSize))
+    (cond
+      ((null? layer) (quote ()))
+      (else (cons layer (getLayers layerSize))))))
 
 ; count the number of ocurrences of `num`
 (define countOccurrences
@@ -52,13 +42,40 @@
       ((= num (car lat)) (countOccurrences (cdr lat) num (add1 ocurrences)))
       (else (countOccurrences (cdr lat) num ocurrences)))))
 
-(define zerosOnLayers
-  (lambda (layerSize)
+(define getZerosOnLayers
+  (lambda (layers)
     (cond
-      (null? (getLayer layerSize) (quote ()))
-      (else (cons (countOccurrences (getLayer layerSize) 0 0) (zerosOnLayers layerSize))))))
+      ((null? layers) (quote ()))
+      (else (cons
+        (countOccurrences (car layers) 0 0)
+        (getZerosOnLayers (cdr layers)))))))
+
+(define getLayerWithOcurrences
+  (lambda (occurrences layers)
+    (define currentLayer (car layers))
+    (define ocurrencesOfCurrent (countOccurrences currentLayer 0 0))
+    (cond
+      ((= ocurrencesOfCurrent occurrences) currentLayer)
+      (else (getLayerWithOcurrences occurrences (cdr layers))))))
+
+; get the min value out of a list of numbers
+(define meen
+  (lambda (lat minVal)
+    (cond 
+      ((null? lat) minVal)
+      ((< (car lat) minVal) (meen (cdr lat) (car lat)))
+      (else (meen (cdr lat) minVal)))))
 
 (define (main args)
   (define layerSize (* rowCount columnCount))
-  (print (zerosOnLayers layerSize))
+  (define layers (getLayers layerSize)) 
+  (define zerosOnLayers (getZerosOnLayers layers))
+
+  ; start assuming the first value of the list is the minimum
+  (define minimum (meen zerosOnLayers (car zerosOnLayers)))
+  (define layerOfInterest (getLayerWithOcurrences minimum layers))
+  (define result (*
+    (countOccurrences layerOfInterest 1 0)
+    (countOccurrences layerOfInterest 2 0)))
+  (print result)
   (file-close infn))
